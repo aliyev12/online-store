f3bac7324e5bf8dda1fab43325a802a03f1eb36b
Abdul Aliyev <aaa7c4@gmail.com>
2020-01-26 21:31:54 -0500

    Fix please sign in

--- frontend/pages/orders.js
+++ frontend/pages/orders.js
@@ -1,10 +1,10 @@
-import PleaseSignIn from '../components/PleaseSignIn';
-import OrderList from '../components/OrderList';
+import PleaseSignIn from "../components/PleaseSignin";
+import OrderList from "../components/OrderList";
 
 export default ({ query }) => (
-    <div>
-        <PleaseSignIn>
-            <OrderList />
-        </PleaseSignIn>
-    </div>
+  <div>
+    <PleaseSignIn>
+      <OrderList />
+    </PleaseSignIn>
+  </div>
 );
